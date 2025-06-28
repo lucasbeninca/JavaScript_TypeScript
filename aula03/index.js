6dca40b70b491ae8f34333fc2ff17fe6c89cca7e
Lucas Beninca <lucasbeninca@proton.me>
2025-06-28 16:38:56 -0300

    init

---
+++ aula03/index.js
@@ -0,0 +1,3 @@
+console.log("teste 'aspas'"); // String com aspas simples
+console.log('teste "aspas duplas"'); // String com aspas duplas
+console.log(`teste "asplas duplas dentor de crase"`); // String com aspas duplas dentro de crase
