5e0cef1278904116fb8ac048e0f0e3784cf0ae84
Lucas Beninca <lucasbeninca@proton.me>
2025-07-01 07:09:29 -0300

    tipos primitidos

---
+++ aula09/index.js
@@ -0,0 +1,12 @@
+const nome = 'João'; // String
+const num1 = 30;    // Number
+let nomeAluno;  // Undefined não aponta para lugar nenhum na memória
+const nomeDoMeioAluno = null; // Null não aponta para lugar nenhum na memória
+const aprovado = true; // Boolean = true or false
+const array = [1, 2, 3]; // Array é um objeto especial
+const objeto = {
+    nome: 'Maria',
+    idade: 25
+}; // Objeto é uma coleção de pares chave-valor
+
+console.log(typeof nome, typeof num1, typeof nomeAluno, typeof nomeDoMeioAluno, typeof aprovado);
