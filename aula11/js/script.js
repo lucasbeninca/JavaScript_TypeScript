2aaa0346b4f475353b4872d034a590a1ae32c23b
Lucas Beninca <lucasbeninca@proton.me>
2025-07-01 22:12:06 -0300

    aula 11 - convertendo valores

---
+++ aula11/js/script.js
@@ -0,0 +1,19 @@
+// alert("Olá, mundo!");
+// funções do navegador
+// window.alert("Olá, mundo!");
+// window.confirm("Você tem certeza?");
+// window.prompt("Digite seu nome:");
+
+
+// let confirma = confirm("Você tem certeza?");
+// let num1 = prompt("Digite um número:");
+
+// Exercicio
+
+const num2 = prompt("Digite o primeiro número:");
+const num3 = prompt("Digite o segundo número:");
+
+
+const soma = Number(num2) + Number(num3);
+
+alert(`A soma dos números é: ${soma}`);
